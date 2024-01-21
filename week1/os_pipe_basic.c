38330e4c449c3ea2338802d883b90617f57874c6
Dhakshin A V <avdhakshin1354@gmail.com>
2024-01-21 10:08:35 +0530

    aadded comments os_pipe_basic.c

--- week1/os_pipe_basic.c
+++ week1/os_pipe_basic.c
@@ -1,32 +1,37 @@
 
 #include<stdio.h>
-#include<unistd.h>
-#include<sys/types.h>
+#include<unistd.h> //for using fork() and pipe()
+#include<sys/types.h> 
 
 int main()
 {
-	char iobuff[20],mes[20];
-	int p[2];
+	char iobuff[20],mes[20]; //for storing buffer message
+	int p[2]; //for creating a pipe
 	pid_t pid;
-	if(pipe(p)<0)
+	if(pipe(p)<0) //creating pipe and if failed exit
 	{
 		return 1;
 	}
-	pid=fork();
-	if(pid>0)
+	pid=fork(); //creating child
+	if(pid>0) //Parent process
 	{
 		printf("In the Parent Process!\n");
 		printf("Enter the message to child process:-\n");
+		//getting message from user
 		scanf("%s",iobuff);
+		//write to child using pipe and write() fns
 		write(p[1],iobuff,20);
 		printf("Exiting Parent!\n");
+		//waits till child executes
 		sleep(5);
 	}
 	else
 	{
+		//to waits till message recived at parent
 		sleep(3);
 		printf("Entering the child Process!\n");
 		printf("Reading the data sent by parent...\n");
+		//reading data that was sent via pipe
 		read(p[0],iobuff,20);
 		printf("Message: %s\n\n",iobuff);
 		printf("Exiting Child!\n");
