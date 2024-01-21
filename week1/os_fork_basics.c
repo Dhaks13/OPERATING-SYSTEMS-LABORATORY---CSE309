380157b1e9ffaa2211f06543a74ab32b30d8b674
Dhakshin A V <avdhakshin1354@gmail.com>
2024-01-21 10:03:58 +0530

    added comments os_fork_basics.c

--- week1/os_fork_basics.c
+++ week1/os_fork_basics.c
@@ -1,19 +1,19 @@
 #include<stdio.h>
-#include<unistd.h>
-#include<sys/types.h>
+#include<unistd.h> //for using fork() 
+#include<sys/types.h> //for pid_t datatype
 
 int main()
 {
-	pid_t p=fork();
-	if(p>0)
+	pid_t p=fork(); //creating child process
+	if(p>0) //parent process condition
 	{
 		printf("In the Parent Process=%d!\n",getpid());
 	}
-	else if(p<0)
+	else if(p<0) //failed condition
 	{
 		printf("Fork failed\n");
 	}
-	else
+	else //child process
 	{
 		printf("In the child Process=%d!\n",getpid());
 	}
